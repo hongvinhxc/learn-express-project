d29785da2be50e33acf48a785e2feb4212ab9274
Herry Vinh <hongvinhxc@gmail.com>
2019-05-07 21:11:22 +0700

    Add debug Node.js app and add Upload file function

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const multer  = require('multer');
 const shortid = require('shortid');
+
 const db = require('../db');
 const controller = require('../controller/user.controller');
 const validate = require('../validate/user.validation');
 
 const router = express.Router();
+const upload = multer({ dest: './public/uploads/' });
+
 
 router.get('/cookie', function(req, res) {
 	res.send('Hello');
@@ -18,6 +22,6 @@ router.get('/create', controller.create);
 
 router.get('/view/:id', controller.viewUser);
 
-router.post('/create', validate.middleware, controller.postCreate);
+router.post('/create', upload.single('avatar'), validate.middleware, controller.postCreate);
 
 module.exports = router;
