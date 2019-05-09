c3f0a200116811d2c44db737bf2371576104d6a3
Herry Vinh <hongvinhxc@gmail.com>
2019-05-09 15:53:03 +0700

    add maxAge for session

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -4,7 +4,7 @@ const db = require('../db');
 module.exports = function(req, res, next) {
 	if (!req.signedCookies.sessionId) {
 		let sessionId = shortid.generate();
-		res.cookie('sessionId', sessionId, {signed: true});
+		res.cookie('sessionId', sessionId, {signed: true, maxAge: 259200});
 		db.get('sessions').push({id: sessionId}).write();
 	}
 	next();
