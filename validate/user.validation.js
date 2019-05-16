edd8cec0f14b82cfc457a31226606c7b6e554c14
hongvinhxc <39236192+hongvinhxc@users.noreply.github.com>
2019-05-16 12:09:59 +0700

    Update user.validation.js
    validation for upload file

--- validate/user.validation.js
+++ validate/user.validation.js
@@ -2,6 +2,7 @@ module.exports.middleware = function(req, res, next) {
 	var errors = [];
 	if (!req.body.name) errors.push("Name is required.");
 	if (!req.body.phone) errors.push("Phone is required.");
+	if (!req.file) errors.push("Avatar is required.");
 	if (errors.length) {
 		res.render('users/create', {
 			error: errors,
